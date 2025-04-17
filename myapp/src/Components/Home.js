853423f69b0a31d375904d54a3766e2f23808fe0
Prashant2515 <prashantpawar952@gmail.com>
2025-04-17 17:49:45 +0530

    Created Components

---
+++ myapp/src/Components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+ const Home = () =>{
+  return(
+    <h1>Home Component</h1>
+  )
+}
+
+const About = () =>{
+  return(
+    <he>About</he>
+  )
+}
+
+export  {Home ,About};
+   
